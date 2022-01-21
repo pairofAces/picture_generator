cd1a6f5557572c927048ec7b385c14c924abb171
pairofAces <karanchauhan16@gmail.com>
2022-01-20 21:08:34 -0500

    creating object that will interact with generated image

--- picture_generator.py
+++ picture_generator.py
@@ -1,12 +1,20 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 def picture_generator():
     print("Generating Picture!")
-    
+
     image_size_px = 128
     image_bg_color = (255, 255, 255)
 
-    image = Image.new("RGB", (image_size_px, image_size_px), image_bg_color)
+    image = Image.new(
+        "RGB", 
+        size=(image_size_px, image_size_px), 
+        color=image_bg_color)
+
+    #Draw lines on the image
+    draw = ImageDraw.Draw(image)
+    draw.line()
+
     image.save("test_image.png")
 
 if __name__ == "__main__":
