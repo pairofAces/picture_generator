7636f7bb8bc27e8e0848319bb7e1ae9c9b4b1c5e
pairofAces <karanchauhan16@gmail.com>
2022-01-20 20:51:34 -0500

    successfully generated white png file

--- picture_generator.py
+++ picture_generator.py
@@ -2,6 +2,8 @@ from PIL import Image
 
 def picture_generator():
     print("Generating Picture!")
+    image = Image.new("RGB", (128, 128), (255, 255, 255))
+    image.save("test_image.png")
 
 if __name__ == "__main__":
     picture_generator()
