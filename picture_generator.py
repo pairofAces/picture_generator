59f58001765edb9cc286c45ee5f8efdc1a538a1b
pairofAces <karanchauhan16@gmail.com>
2022-01-20 20:53:55 -0500

    created variables for pixel and background color requirements

--- picture_generator.py
+++ picture_generator.py
@@ -2,7 +2,11 @@ from PIL import Image
 
 def picture_generator():
     print("Generating Picture!")
-    image = Image.new("RGB", (128, 128), (255, 255, 255))
+    
+    image_size_px = 128
+    image_bg_color = (255, 255, 255)
+
+    image = Image.new("RGB", (image_size_px, image_size_px), image_bg_color)
     image.save("test_image.png")
 
 if __name__ == "__main__":
